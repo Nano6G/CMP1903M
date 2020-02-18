316c08ef590ffda994437b6a38fe00e12bf1e2ff
William James Smith <19693578@students.lincoln.ac.uk>
2020-02-18 16:00:35 +0000

    Main class that reads file line by line

--- CMP1903M/Program.cs
+++ CMP1903M/Program.cs
@@ -1,21 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CMP1903M
 {
     class Country
     {
         string name;
-        float population;
+        double population;
+        int vote;
 
-        public void SetCountry(string CountryName, float CountryPopulation)
+        public Country(string CountryName, double CountryPopulation, int CountryVote)
         {
             name = CountryName;
             population = CountryPopulation;
+            vote = CountryVote;
         }
 
         public string GetCountry()
         {
             return name;
+        }
+    }
+
+    class MainClass
+    {
+        static void Main()
+        {
+            string line;
+
+            List<Country> Countrys = new List<Country>();
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\willz\Desktop\OOP\CMP1903M\CMP1903M\Countries.txt");
+            //System.IO.StreamReader file = new System.IO.StreamReader(Path.Combine(Directory.GetCurrentDirectory(), "\\Countries.txt")); 
+            //System.IO.StreamReader file = new System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory.ToString(), "Countries.txt"));
+
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+            }
         }
     }
 }
