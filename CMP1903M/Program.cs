7129f280776209286a9f96b87c5b4cfa1fe111e5
William James Smith <19693578@students.lincoln.ac.uk>
2020-02-19 17:20:06 +0000

    Countries read line by line and created into objects.

--- CMP1903M/Program.cs
+++ CMP1903M/Program.cs
@@ -10,16 +10,21 @@ namespace CMP1903M
         double population;
         int vote;
 
-        public Country(string CountryName, double CountryPopulation, int CountryVote)
+        public Country(string CountryName, string CountryPopulation)
         {
             name = CountryName;
-            population = CountryPopulation;
-            vote = CountryVote;
+            population = Convert.ToDouble(CountryPopulation);
+            vote = 0;
         }
 
         public string GetCountry()
         {
             return name;
+        }
+
+        public string GetCountryPop()
+        {
+            return Convert.ToString(population) + "%";
         }
     }
 
@@ -35,9 +40,20 @@ namespace CMP1903M
             //System.IO.StreamReader file = new System.IO.StreamReader(Path.Combine(Directory.GetCurrentDirectory(), "\\Countries.txt")); 
             //System.IO.StreamReader file = new System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory.ToString(), "Countries.txt"));
 
+            //List<String> line_strings = new List<String>();
+
             while ((line = file.ReadLine()) != null)
             {
-                System.Console.WriteLine(line);
+                //line_strings.Add(line);
+                string[] CountryDetails = line.Split(",");
+                Countrys.Add(new Country(CountryDetails[0], CountryDetails[1]));
+            }
+
+            Console.WriteLine("Country" + " ".PadLeft(13) + "Population");
+
+            foreach(Country x in Countrys)
+            {
+                Console.WriteLine(x.GetCountry() + " ".PadLeft(20-x.GetCountry().Length) + x.GetCountryPop());
             }
         }
     }
